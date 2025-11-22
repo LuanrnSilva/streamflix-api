0ca7c906a915ec55eae9db9150890198992da543
Luan Silva <luan.nascimento@segsat.com>
2025-11-22 09:37:50 -0300

    add series on my list

--- streamflix/services.py
+++ streamflix/services.py
@@ -1,21 +1,19 @@
 import requests
 
-TMDB_API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxODVmYTVkNmY5M2UwODJlMWMzYjkyYTU5MzgyZTYxOCIsIm5iZiI6MTczNzUwNjA0MC43MTIsInN1YiI6IjY3OTAzY2Y4YmU5YTlhYTc4Mjc3NDA5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Jkkt9V0WEXsWNVmQjx7QJeyYmvrBlKxyfPCYUjbAcfM";  # coloque a sua
+TMDB_API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxODVmYTVkNmY5M2UwODJlMWMzYjkyYTU5MzgyZTYxOCIsIm5iZiI6MTczNzUwNjA0MC43MTIsInN1YiI6IjY3OTAzY2Y4YmU5YTlhYTc4Mjc3NDA5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Jkkt9V0WEXsWNVmQjx7QJeyYmvrBlKxyfPCYUjbAcfM"
 
 class TMDBService:
-    BASE = "https://api.themoviedb.org/3/movie/"
-
     @classmethod
-    def fetch_movie(cls, tmdb_id):
-        url = f"{cls.BASE}{tmdb_id}"
+    def fetch_content(cls, tmdb_id):
+        headers = {"Authorization": f"Bearer {TMDB_API_KEY}"}
+        params = {"language": "pt-BR"}
 
-        resp = requests.get(url, 
-            headers={
-                "Authorization": f"Bearer {TMDB_API_KEY}", 
-            },
-            params={
-                "language": "pt-BR"
-            }
-        )
+        resp = requests.get(f"https://api.themoviedb.org/3/movie/{tmdb_id}", headers=headers, params=params)
+        if resp.status_code == 200:
+            return resp.json()
+
+        resp = requests.get(f"https://api.themoviedb.org/3/tv/{tmdb_id}", headers=headers, params=params)
+        if resp.status_code == 200:
+            return resp.json()
+
         resp.raise_for_status()
-        return resp.json()
